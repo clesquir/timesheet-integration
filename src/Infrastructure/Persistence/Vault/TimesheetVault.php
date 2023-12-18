bff43d415775bac0e433560b49523f746c62c706
Cédric Lesquir <clesquir@devalto.com>
2023-12-18 14:26:56 -0500

    Update base URL

--- src/Infrastructure/Persistence/Vault/TimesheetVault.php
+++ src/Infrastructure/Persistence/Vault/TimesheetVault.php
@@ -6,6 +6,6 @@ final readonly class TimesheetVault {
 	const KEYCLOAK_DEVICE_AUTH = 'https://keycloak.deval.to/realms/DevAlto/protocol/openid-connect/auth/device';
 	const KEYCLOAK_TOKEN = 'https://keycloak.deval.to/realms/DevAlto/protocol/openid-connect/token';
 	const KEYCLOAK_CLIENT_ID = 'timesheet';
-	const BASE_URL = 'https://devalto-stg.timesheet.wtf';
+	const BASE_URL = 'https://devalto.timesheet.wtf';
 	const CREDENTIALS_FILE = '/tmp/timesheet-credentials.json';
 }
